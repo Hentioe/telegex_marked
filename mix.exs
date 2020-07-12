2a338ad9d4b2eef58872708f50889741dfebcb31
Hentioe <me@bluerain.io>
2020-07-12 21:21:32 +0800

    Release v0.0.6

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Telegex.Marked.MixProject do
   use Mix.Project
   @description "Safe Markdown parser/renderer for Telegram."
 
-  @version "0.0.5"
+  @version "0.0.6"
 
   def project do
     [
